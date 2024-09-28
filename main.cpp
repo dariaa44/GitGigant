5a0ba24eee037815d615756fe815e159e069de0e
dariaa44 <dariasalasinska@gmail.com>
2024-09-28 12:15:09 +0200

    czwarty commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
     cout<<"Hello git"<<endl;
     cout<<"Jestem w repozytorium a to jest drugi commit"<<endl;
     cout<<"Trzeci commit na nowym branchu"<<endl;
+    cout<<"Czwarty commit na nowym branchu"<<endl;
 
     return 0;
 }
